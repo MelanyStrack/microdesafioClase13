b11199fcf6843da25da5002715bfb2e43c9470fa
MelanyStrack <melany.strack@gmail.com>
2023-09-17 19:30:52 -0300

    Cree el archivo tiendaDeTecnologia.js

---
+++ tiendaDeTecnologia.js
@@ -0,0 +1,23 @@
+const celulares = [
+    {
+        marca: "Samsung",
+        modelo: "A31",
+        memoria: "64GB",
+        garantia: "6 meses",
+        precio: 90000,
+    },
+    {
+        marca: "Apple",
+        modelo: "Iphone 12",
+        memoria: "128GB",
+        garantia: "3 meses",
+        precio: 320000,
+    },
+    {
+        marca: "Motorola",
+        modelo: "G10",
+        memoria: "64GB",
+        garantia: "4 meses",
+        precio: 62000
+    }
+]
